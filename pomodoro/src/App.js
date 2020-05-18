dfff1148cb90d698ec2160ff0d653c6ed06a3c05
Guillaume-Boeur <guillaume.boeur@gmail.com>
2020-05-18 16:41:26 +0200

    +1 -1 on State

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 // Don't forget to add my futur component as :
 // import Minus from './Minus';
 // import Play from './Play';
@@ -6,14 +6,21 @@ import React, { Component } from "react";
 // import Reset from './Reset';
 // import Time from './Time';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, React!</h1>
-      </div>
-    );
-  }
+//Parent
+const App = () => {
+  const [time, setTime] = useState(0);
+
+  return (
+    <div className="App">
+      <p>{'Il reste : ${time}'}</p>
+      <button onClick={() => setTime(time + 1)}>{"+"}</button>
+      <button onClick={() => setTime(time - 1)}>{"-"}</button>
+      <button>{"Play"}</button>
+      <button>{"Reset"}</button>
+
+    </div>
+  );
+  
 }
 
 export default App;
